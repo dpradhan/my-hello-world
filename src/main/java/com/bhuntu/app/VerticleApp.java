99d6fe3b1af71f8b589a645aefa2b3e6919709cb
Darshan Pradhan <darshan.pradhan@ignitionone.com>
2017-01-02 16:38:05 -0500

    first commit

---
+++ src/main/java/com/bhuntu/app/VerticleApp.java
@@ -0,0 +1,11 @@
+package com.bhuntu.app;
+
+import io.vertx.core.AbstractVerticle;
+
+public class VerticleApp extends AbstractVerticle
+{
+    @Override
+    public void start() throws Exception {
+        vertx.createHttpServer().requestHandler(req -> req.response().end("Hello World Bhuntu!")).listen(8000);
+    }
+}
